e4fe4bdac71a213d187bfb8817a3b19e711d07a5
Jake Robbins <jrobbinsd@gmail.com>
2020-01-12 08:34:11 -0700

    new dependencies

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
 const port = 3000;
 const db = require('./queries');
@@ -10,6 +11,17 @@ app.use(
     extended: true,
   })
 )
+
+app.use(cors({
+			 origin: "*",
+			 allowedHeaders: "Content-type",
+			 methods: "GET,POST,PUT,DELETE,OPTIONS" }));
+
+	 app.use(function(req, res, next) {
+			 res.header("Access-Control-Allow-Origin", "*");
+			 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+			 next();
+	 });
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
