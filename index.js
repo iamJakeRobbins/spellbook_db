ebfe137c7c557e4468f9d46cfe1019fcc4d7b2cc
Jake Robbins <jrobbinsd@gmail.com>
2020-01-12 09:47:51 -0700

    cleanup

--- index.js
+++ index.js
@@ -14,19 +14,6 @@ app.use(
   })
 )
 
-// app.use(cors({
-// 			 origin: "*",
-// 			 allowedHeaders: "Content-type",
-// 			 methods: "GET,POST,PUT,DELETE,OPTIONS" }));
-//
-// 	 app.use(function(req, res, next) {
-// 			 res.header("Access-Control-Allow-Origin", "*");
-// 			 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// 			 next();
-// 	 });
-
-
-
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
