37c5e66e765bcace958fe69f695bf17c7da21bfc
Jake Robbins <jrobbinsd@gmail.com>
2020-01-12 08:59:59 -0700

    fetch is working using cors

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = 3000;
 const db = require('./queries');
 
+app.use(cors());
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -12,16 +14,18 @@ app.use(
   })
 )
 
-app.use(cors({
-			 origin: "*",
-			 allowedHeaders: "Content-type",
-			 methods: "GET,POST,PUT,DELETE,OPTIONS" }));
+// app.use(cors({
+// 			 origin: "*",
+// 			 allowedHeaders: "Content-type",
+// 			 methods: "GET,POST,PUT,DELETE,OPTIONS" }));
+//
+// 	 app.use(function(req, res, next) {
+// 			 res.header("Access-Control-Allow-Origin", "*");
+// 			 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 			 next();
+// 	 });
 
-	 app.use(function(req, res, next) {
-			 res.header("Access-Control-Allow-Origin", "*");
-			 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-			 next();
-	 });
+
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
