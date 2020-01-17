1d715b5ee5556cd98bfbef6188a58c6c187bc198
Jake Robbins <jrobbinsd@gmail.com>
2020-01-17 09:27:22 -0700

    working on query structure

--- queries.js
+++ queries.js
@@ -14,7 +14,6 @@ const getCharacters = (request, response) => {
 		FROM characters
 		ORDER BY id ASC`, (error, results) => {
     if (error) {
-
       throw error
     }
     response.status(200).json(results.rows)
@@ -22,10 +21,18 @@ const getCharacters = (request, response) => {
 };
 
 const insertChar = (req, res) => {
-	console.log(req.body)
-	console.log('hello world')
 	let userId = 44808;
-	// let name = re
+	let data = req.body;
+	pool.query(
+	`INSERT INTO characters
+	(user_id, name, level, class)
+	VALUES ($1,$2,$3,$4)`, (error, results) => {
+		if(error) {
+			console.log('uhoh')
+			throw error
+		}
+		response.status(200);
+	})
 }
 
 module.exports = {
