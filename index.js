e5452e93f46f830c3e12ebda261712a90118c978
Jake Robbins <jrobbinsd@gmail.com>
2020-01-21 16:45:59 -0700

    file clean up

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = 3000;
 const db = require('./queries');
 
 app.use(cors());
-
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -14,14 +13,10 @@ app.use(
   })
 )
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-});
-
 app.get('/chars', db.getCharacters);
-app.post('/getSingleCharacter', db.getSingleCharacter);
 app.get('/classDetails', db.getClassDetails);
 
+app.post('/getSingleCharacter', db.getSingleCharacter);
 app.post('/submitCharacter', db.insertChar);
 app.post('/updateCharacter', db.updateCharacter);
 app.post('/deleteCharacter', db.deleteCharacter);
